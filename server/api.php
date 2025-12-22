0daf369b29e916338a1efd2a9f7e3ea1f160e89d
LangYa466 <langya466@gmail.com>
2025-12-22 14:45:42 +0800

    2

--- server/api.php
+++ server/api.php
@@ -1,8 +1,15 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
-$category = isset($_GET['category']) ? $_GET['category'] : null;
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+$category = isset($input['category']) ? $input['category'] : null;
 
 function getCategoryMeta($dir) {
     $metaFile = $dir . '/_category_.json';
