7adb03da400957af9d2c332ee4fba52500360540
LangYa466 <langya466@gmail.com>
2025-12-22 14:49:01 +0800

    ？

--- server/api.php
+++ server/api.php
@@ -1,12 +1,14 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
+    http_response_code(200);
+    exit;
 }
+
+header('Content-Type: application/json; charset=utf-8');
 
 $input = json_decode(file_get_contents('php://input'), true);
 $category = isset($input['category']) ? $input['category'] : null;
