839239dbf6c1cb9056a4371c139846a23fa94515
LangYa466 <langya466@gmail.com>
2026-01-04 15:01:45 +0800

    HIDE

--- server/api.php
+++ server/api.php
@@ -1,13 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 header('Content-Type: application/json; charset=utf-8');
 
 $input = json_decode(file_get_contents('php://input'), true);
