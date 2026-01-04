83bfbbc81d80d4bccb60a15e0f67dee50a666516
LangYa466 <langya466@gmail.com>
2026-01-04 17:39:05 +0800

    js压缩

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  vite: {
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: false,
+        },
+        format: {
+          comments: false,
+        },
+      },
+    },
+  },
 });
